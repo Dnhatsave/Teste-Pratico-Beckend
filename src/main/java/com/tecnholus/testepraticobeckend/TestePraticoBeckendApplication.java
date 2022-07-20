ca166e27ba31570a5814c2e4a6b1bb6a2811217c
Dnhatsave <dercionhatsave@gmail.com>
2022-07-20 08:27:29 +0200

    Upload do spring Beckend Project

---
+++ src/main/java/com/tecnholus/testepraticobeckend/TestePraticoBeckendApplication.java
@@ -0,0 +1,13 @@
+package com.tecnholus.testepraticobeckend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestePraticoBeckendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestePraticoBeckendApplication.class, args);
+	}
+
+}
